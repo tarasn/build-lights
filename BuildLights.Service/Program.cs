2ba0a7220bbd24bb542e361f45a12afc0c3d541a
TarasN <tarasn@gmail.com>
2018-10-15 01:03:50 +0300

    initial adding

---
+++ BuildLights.Service/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace BuildLights.Service
+{
+    class BuildLightsService
+    {
+        public void Start() {  }
+        public void Stop() {  }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>                                   
+            {
+                x.Service<BuildLightsService>(s =>                                  
+                {
+                    s.ConstructUsing(name => new BuildLightsService());             
+                    s.WhenStarted(tc => tc.Start());                        
+                    s.WhenStopped(tc => tc.Stop());                         
+                });
+                x.RunAsLocalSystem();                                       
+
+                x.SetDescription("Sample Topshelf Host");                   
+                x.SetDisplayName("Stuff");                                  
+                x.SetServiceName("Stuff");                                  
+            });                                                             
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
---
+++ BuildLights.Service/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuildLights.Service")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("BuildLights.Service")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("372c47fd-a4be-47c0-a3d6-88cdd36d8465")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
