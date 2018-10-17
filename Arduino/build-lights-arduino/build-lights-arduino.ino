436b0e49366a9c7787946aec7447ea5ca6e82300
TarasN <tarasn@gmail.com>
2018-10-18 00:04:37 +0300

    updated reading from serial

--- Arduino/build-lights-arduino/build-lights-arduino.ino
+++ Arduino/build-lights-arduino/build-lights-arduino.ino
@@ -1,12 +1,13 @@
-const int redPin = 10;  //0 position
-const int greenPin = 9; //1 position
-const int yellowPin = 8;//2 position
+const int redPin = 9; 
+const int greenPin = 10; 
+const int yellowPin = 8;
 
 const int redFlagPosition = 0;  //0 position
 const int greenFlagPosition = 1; //1 position
 const int yellowFlagPosition = 2;//2 position
 
 byte lightFlags = 0;
+String inString = "";    // string to hold input
 
 void setup() {
  // initialize serial:
@@ -21,12 +22,39 @@ void setup() {
 void loop() {
   //if there's any serial available, read it:
   while (Serial.available() > 0) {
+
+    int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      // convert the incoming byte to a char and add it to the string:
+      inString += (char)inChar;
+    }
+    // if you get a newline, print the string, then the string's value:
+    if (inChar == '\n') {
+      /*Serial.print("Value:");
+      Serial.println(inString.toInt());
+      Serial.print("String: ");
+      Serial.println(inString);
+      // clear the string for new input:
+      inString = "";*/
+      Serial.print("Value:");
+      Serial.println(inString.toInt());
+      Serial.print("String: ");
+      Serial.println(inString);
+      lightFlags = (byte)inString.toInt();
+      setLightPinState(redPin, redFlagPosition);
+      setLightPinState(greenPin, greenFlagPosition);
+      setLightPinState(yellowPin, yellowFlagPosition);
+      inString = "";
+    }
+
+    /*
     lightFlags = (byte)Serial.parseInt();
     if (Serial.read() == '\n') {
       setLightPinState(redPin, redFlagPosition);
       setLightPinState(greenPin, greenFlagPosition);
       setLightPinState(yellowPin, yellowFlagPosition);
     }
+    */
   }
 }
 
