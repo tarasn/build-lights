8e4c5c970b2ac4928597a049e0144de9e5a4289e
TarasN <tarasn@gmail.com>
2018-10-20 23:29:47 +0300

    finished player

--- BuildLights.WinAppTester/Form1.Designer.cs
+++ BuildLights.WinAppTester/Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.buttonSend = new System.Windows.Forms.Button();
-            this.checkedListBoxColors = new System.Windows.Forms.CheckedListBox();
+            this.checkBoxYellow = new System.Windows.Forms.CheckBox();
+            this.checkBoxGreen = new System.Windows.Forms.CheckBox();
+            this.checkBoxRed = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // buttonSend
@@ -42,37 +44,62 @@
             this.buttonSend.UseVisualStyleBackColor = true;
             this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
             // 
-            // checkedListBoxColors
+            // checkBoxYellow
             // 
-            this.checkedListBoxColors.Dock = System.Windows.Forms.DockStyle.Top;
-            this.checkedListBoxColors.FormattingEnabled = true;
-            this.checkedListBoxColors.Items.AddRange(new object[] {
-            "Red",
-            "Green",
-            "Yellow"});
-            this.checkedListBoxColors.Location = new System.Drawing.Point(0, 0);
-            this.checkedListBoxColors.Name = "checkedListBoxColors";
-            this.checkedListBoxColors.Size = new System.Drawing.Size(284, 214);
-            this.checkedListBoxColors.TabIndex = 1;
+            this.checkBoxYellow.AutoSize = true;
+            this.checkBoxYellow.Location = new System.Drawing.Point(28, 23);
+            this.checkBoxYellow.Name = "checkBoxYellow";
+            this.checkBoxYellow.Size = new System.Drawing.Size(57, 17);
+            this.checkBoxYellow.TabIndex = 1;
+            this.checkBoxYellow.Text = "Yellow";
+            this.checkBoxYellow.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxGreen
+            // 
+            this.checkBoxGreen.AutoSize = true;
+            this.checkBoxGreen.Location = new System.Drawing.Point(28, 69);
+            this.checkBoxGreen.Name = "checkBoxGreen";
+            this.checkBoxGreen.Size = new System.Drawing.Size(55, 17);
+            this.checkBoxGreen.TabIndex = 2;
+            this.checkBoxGreen.Text = "Green";
+            this.checkBoxGreen.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRed
+            // 
+            this.checkBoxRed.AutoSize = true;
+            this.checkBoxRed.Location = new System.Drawing.Point(28, 116);
+            this.checkBoxRed.Name = "checkBoxRed";
+            this.checkBoxRed.Size = new System.Drawing.Size(46, 17);
+            this.checkBoxRed.TabIndex = 3;
+            this.checkBoxRed.Text = "Red";
+            this.checkBoxRed.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 381);
-            this.Controls.Add(this.checkedListBoxColors);
+            this.Controls.Add(this.checkBoxRed);
+            this.Controls.Add(this.checkBoxGreen);
+            this.Controls.Add(this.checkBoxYellow);
             this.Controls.Add(this.buttonSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Build Lights Arduino Tester";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonSend;
-        private System.Windows.Forms.CheckedListBox checkedListBoxColors;
+        private System.Windows.Forms.CheckBox checkBoxYellow;
+        private System.Windows.Forms.CheckBox checkBoxGreen;
+        private System.Windows.Forms.CheckBox checkBoxRed;
     }
 }
 
